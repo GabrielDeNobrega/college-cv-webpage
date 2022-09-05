f176d35afd6cae7846b8298eb667c975c50491d1
GabrielDeNobrega <sr.gabrielnobrega@hotmail.com>
2022-09-05 18:46:15 -0300

    Functionalities
    Iniciei a construção dos scripts utilizando JavaScript.

---
+++ js/index.js
@@ -0,0 +1,11 @@
+let text = '{ "person" : [' +
+    '{ "firstName":"Gabriel " , "lastName":"de Nóbrega Araújo", '
+    + '"email":"sr.gabrielnobrega@hotmail.com", "phone":"+55 (011) 98270-7597", "location":"São Paulo, Brazil",' 
+    + '"goal":"Java Developer", "linkedin":"https://br.linkedin.com/in/gabriel-n%C3%B3brega-9b8260169?original_referer=https%3A%2F%2Fwww.google.com%2F",' 
+    + '"github":"https://github.com/GabrielDeNobrega" } ]}';
+
+
+const obj = JSON.parse(text);
+
+document.getElementById("main-title").innerHTML =
+obj.person.lastName + " " + obj.person.lastName;
