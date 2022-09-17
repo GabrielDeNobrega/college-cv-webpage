3705d20957be6f3562c470f901776e8881967b74
GabrielDeNobrega <sr.gabrielnobrega@hotmail.com>
2022-09-17 16:47:03 -0300

    Reading JSON
    I've made it so the webpage reads a generic JSON.

--- js/index.js
+++ js/index.js
@@ -1,60 +1,77 @@
-import file from "./infos.js";
+var obj = {};
 
-const obj = JSON.parse(file);
+var x = new XMLHttpRequest();
 
-document.getElementById("main-title").innerHTML =
-    obj.person[0].informations.fullName;
+x.open('GET', './js/infos.json', true);
+x.onreadystatechange = function () {
+    if (x.readyState == 4) {
+        console.log(x.responseText)
+        obj = JSON.parse(x.responseText);
+        fillPage(obj);
+    }
+};
 
-document.getElementById("email").innerHTML =
-    obj.person[0].informations.email;
+x.send();
 
-document.getElementById("phone").innerHTML =
-    obj.person[0].informations.phone;
+function fillPage(obj) {
 
-document.getElementById("location").innerHTML =
-    obj.person[0].informations.location;
+    document.getElementById("main-title").innerHTML =
+        obj.person[0].informations.fullName;
 
-document.getElementById("goal").innerHTML =
-    obj.person[0].informations.goal;
+    document.getElementById("email").innerHTML =
+        obj.person[0].informations.email;
 
-document.getElementById("career-first").innerHTML =
-    obj.person[0].experience.first;
+    document.getElementById("phone").innerHTML =
+        obj.person[0].informations.phone;
 
-document.getElementById("career-second").innerHTML =
-    obj.person[0].experience.second;
+    document.getElementById("location").innerHTML =
+        obj.person[0].informations.location;
 
-document.getElementById("career-third").innerHTML =
-    obj.person[0].experience.third;
+    document.getElementById("goal").innerHTML =
+        obj.person[0].informations.goal;
 
-document.getElementById("education-first").innerHTML =
-    obj.person[0].education.first;
+    document.getElementById("career-first").innerHTML =
+        obj.person[0].experience.first;
 
-document.getElementById("education-second").innerHTML =
-    obj.person[0].education.second;
+    document.getElementById("career-second").innerHTML =
+        obj.person[0].experience.second;
 
-document.getElementById("abilities").innerHTML =
-    obj.person[0].abilities.first;
+    document.getElementById("career-third").innerHTML =
+        obj.person[0].experience.third;
 
-document.getElementById("first-language-name").innerHTML =
-    obj.person[0].languages.first.name;
+    document.getElementById("education-first").innerHTML =
+        obj.person[0].education.first;
 
-document.getElementById("first-speaking").innerHTML =
-    obj.person[0].languages.first.speaking;
-document.getElementById("first-reading").innerHTML =
-    obj.person[0].languages.first.reading;
-document.getElementById("first-writing").innerHTML =
-    obj.person[0].languages.first.writing;
-document.getElementById("first-listening").innerHTML =
-    obj.person[0].languages.first.listening;
+    document.getElementById("education-second").innerHTML =
+        obj.person[0].education.second;
 
-document.getElementById("second-language-name").innerHTML =
-    obj.person[0].languages.second.name;
+    document.getElementById("abilities").innerHTML =
+        obj.person[0].abilities.first;
 
-document.getElementById("second-speaking").innerHTML =
-    obj.person[0].languages.second.speaking;
-document.getElementById("second-reading").innerHTML =
-    obj.person[0].languages.second.reading;
-document.getElementById("second-writing").innerHTML =
-    obj.person[0].languages.second.writing;
-document.getElementById("second-listening").innerHTML =
-    obj.person[0].languages.second.listening;
+    document.getElementById("first-language-name").innerHTML =
+        obj.person[0].languages.first.name;
+
+    document.getElementById("first-speaking").innerHTML =
+        obj.person[0].languages.first.speaking;
+    document.getElementById("first-reading").innerHTML =
+        obj.person[0].languages.first.reading;
+    document.getElementById("first-writing").innerHTML =
+        obj.person[0].languages.first.writing;
+    document.getElementById("first-listening").innerHTML =
+        obj.person[0].languages.first.listening;
+
+    document.getElementById("second-language-name").innerHTML =
+        obj.person[0].languages.second.name;
+
+    document.getElementById("second-speaking").innerHTML =
+        obj.person[0].languages.second.speaking;
+    document.getElementById("second-reading").innerHTML =
+        obj.person[0].languages.second.reading;
+    document.getElementById("second-writing").innerHTML =
+        obj.person[0].languages.second.writing;
+    document.getElementById("second-listening").innerHTML =
+        obj.person[0].languages.second.listening;
+
+}
+
+
